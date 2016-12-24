772948ca66567fbb0b24787bded251c6bd47db49
John Kurlak <john@kurlak.com>
2016-12-24 09:57:27 -0800

    Add gulp watch to rebuild JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+
+const CLIENT_JS_PATH = 'src/client/**/*.js';
 
 gulp.task('default', () => {
     return gulp
-        .src('src/client/**/*.js')
+        .src(CLIENT_JS_PATH)
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(concat('client.js'))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+    return watch(CLIENT_JS_PATH, () => {
+        gulp.start('default');
+    });
 });
