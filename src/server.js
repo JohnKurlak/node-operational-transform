f19f94f197a680a66ca78876e05627ba7acf04c8
John Kurlak <john@kurlak.com>
2016-12-23 22:25:20 -0800

    Add demo file and setup socket.io

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const port = 80;
+const http = require('http');
+const socketio = require('socket.io');
 
-app.get('/', (request, response) => {
-    response.send('Hello world!');
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log('Server listening on port ' + port + '!');
 });
 
-app.listen(port, () => {
-    console.log('Server listening on port ' + port + '!');
+io.on('connection', socket => {
+    // TODO
 });
