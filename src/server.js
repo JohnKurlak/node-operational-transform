da86e19d70ca7f1f4d6698dc2f28b602353135a3
John Kurlak <John Kurlak>
2016-12-23 21:08:55 -0800

    Add express and socket.io

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Server');
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (request, response) => {
+    response.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port + '!');
+});
