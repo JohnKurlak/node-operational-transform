4f67edfffc6a5ef33d2c1dc68b7f7b92b017a884
John Kurlak <john@kurlak.com>
2016-12-31 15:33:30 -0800

    Initial support for insert OT

--- src/client/client.js
+++ src/client/client.js
@@ -7,10 +7,13 @@
 
     class NodeOTClient {
         constructor(socketIOEndpoint, id) {
-            this.socketIOEndpoint = socketIOEndpoint;
-            this.id = id;
+            this._socketIOEndpoint = socketIOEndpoint;
+            this._id = id;
             this._handlers = {};
-            this.version = 0;
+            this._otDocument = new OperationalTransformation(
+                operation => this._sendServer('push', operation),
+                operation => this._sendClient('apply', operation),
+            );
 
             this._connect();
             this._listen();
@@ -20,33 +23,42 @@
             return new NodeOTClient(socketIOEndpoint, id);
         }
 
-        commit(operation) {
-            console.log('pushing', operation);
-            this._sendServer('push', operation);
-        }
-
         on(eventName, handler) {
             this._handlers[eventName] = this._handlers[eventName] || [];
             this._handlers[eventName].push(handler);
         }
 
+        commit(operation) {
+            console.log('pushing', operation);
+            this._otDocument.commit(operation);
+        }
+
         _merge(version, operation) {
             console.log('merging', operation);
-            this.version = version;
-            this._sendClient('merge', operation);
+            console.log('old version=', this._otDocument.version, 'new version=', version);
+
+            this._otDocument.merge(version, operation);
+        }
+
+        _merged(version) {
+            console.log('merged');
+            console.log('old version=', this._otDocument.version, 'new version=', version);
+
+            this._otDocument.merged(version);
         }
 
         _connect() {
-            this.socket = io.connect(this.socketIOEndpoint);
+            this.socket = io.connect(this._socketIOEndpoint);
             this._sendServer('join');
         }
 
         _listen() {
             this.socket.on('merge', this._merge.bind(this));
+            this.socket.on('merged', this._merged.bind(this));
         }
 
         _sendServer(eventName, ...args) {
-            this.socket.emit(eventName, this.id, this.version, ...args);
+            this.socket.emit(eventName, this._id, this._otDocument.version, ...args);
         }
 
         _sendClient(eventName, ...args) {
@@ -55,6 +67,81 @@
             handlers.forEach(handler => {
                 handler(...args);
             });
+        }
+    }
+
+    // TODO: Move into separate file
+    class OperationalTransformation {
+        constructor(sendToServer, applyToClient) {
+            this.version = 0;
+            this.operations = [];
+            this.pendingOperations = [];
+            this._sendToServer = sendToServer;
+            this._applyToClient = applyToClient;
+        }
+
+        commit(operation) {
+            if (this.pendingOperations.length === 0) {
+                this._sendToServer(operation);
+            }
+
+            this.pendingOperations.push(operation);
+        }
+
+        merge(version, operation) {
+            for (let i = this.pendingOperations.length - 1; i >= 0; --i) {
+                const inverseOperation = this._inverse(this.pendingOperations[i]);
+                this._applyToClient(inverseOperation);
+            }
+
+            this.version = version;
+            this.operations.push(operation);
+
+            this._applyToClient(operation);
+
+            for (let i = 0; i < this.pendingOperations.length; ++i) {
+                this._transform(this.pendingOperations[i], operation);
+                this._applyToClient(this.pendingOperations[i]);
+            }
+        }
+
+        merged(version) {
+            this.version = version;
+            this.operations.push(this.pendingOperations.shift());
+
+            if (this.pendingOperations.length > 0) {
+                this._sendToServer(this.pendingOperations[0]);
+            }
+        }
+
+        _inverse(operation) {
+            const isInsert = this._isInsert(operation);
+
+            return {
+                type: isInsert ? 'delete' : 'insert',
+                text: operation.text,
+                start: operation.start,
+                end: isInsert ? operation.start + operation.text.length : operation.start,
+            };
+        }
+
+        _transform(newOperation, oldOperation) {
+            if (this._isInsert(newOperation) && this._isInsert(oldOperation)) {
+                if (newOperation.start === newOperation.end && oldOperation.start === oldOperation.end) {
+                    if (oldOperation.start < newOperation.start) {
+                        newOperation.start += oldOperation.text.length;
+                        newOperation.end = newOperation.start;
+                    }
+                }
+            }
+        }
+
+        _isInsert(operation) {
+            return (operation.type === 'insert');
+        }
+
+        _isDelete(operation) {
+            return (operation.type === 'delete');
         }
     }
 
--- src/server/server.js
+++ src/server/server.js
@@ -29,26 +29,80 @@ io.on('connection', socket => {
     socket.on('push', (id, version, operation) => {
         const otDocument = otDocuments[id];
         if (!otDocument) {
-            // Fow now, fail gracefully
+            // TODO: Handle failures better; for now, fail gracefully
             return;
         }
 
         console.log('Client (v. ' + version + ') pushed', operation, 'to server (v. ' + otDocument.version + ')');
+        console.log('Non transformed operation is', operation.type, 'with start=', operation.start, 'and end=', operation.end);
 
         otDocument.merge(version, operation);
+
+        console.log('Transformed operation is    ', operation.type, 'with start=', operation.start, 'and end=', operation.end);
+
+        socket.emit('merged', otDocument.version);
         socket.to(id).broadcast.emit('merge', otDocument.version, operation);
     });
 });
 
+// TODO: Move into separate file
 class OperationalTransformation {
     constructor() {
-        this.version = 0;
-        this.operations = [];
+        this._version = 0;
+        this._operations = [];
+    }
+
+    get version() {
+        return this._version;
     }
 
     merge(version, operation) {
-        ++this.version;
-        this.operations.push(operation);
+        for (let i = version; i < this._version; ++i) {
+            this._transform(operation, this._operations[i]);
+        }
+
+        this._operations.push(operation);
+        ++this._version;
         return operation;
+    }
+
+    _transform(newOperation, oldOperation) {
+        // TODO: Add support for more operation types
+
+        if (this._isInsert(newOperation) && this._isInsert(oldOperation)) {
+            if (newOperation.start === newOperation.end && oldOperation.start === oldOperation.end) {
+                if (oldOperation.start < newOperation.start) {
+                    newOperation.start += oldOperation.text.length;
+                    newOperation.end = newOperation.start;
+                }
+            }
+        }
+    }
+
+    // TODO: Use this
+    _getDeleteRange(operation) {
+        let deleteStart = operation.start;
+        let deleteEnd = operation.end;
+
+        if (this._isDelete(operation) && deleteStart === deleteEnd) {
+            deleteStart = Math.max(0, deleteStart - 1);
+        }
+
+        return {
+            start: deleteStart,
+            end: deleteEnd,
+        };
+    }
+
+    _getInsertPosition(operation) {
+        return operation.start;
+    }
+
+    _isInsert(operation) {
+        return (operation.type === 'insert');
+    }
+
+    _isDelete(operation) {
+        return (operation.type === 'delete');
     }
 }
